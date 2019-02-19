c18bd98d6085a16808190b430c0f47325a60e91f
Hajime Todo <samsepylot@gmail.com>
2019-02-19 18:52:29 +0900

    ファイルの削除

--- db/migrate/20190215091852_create_labels.rb
+++
@@ -1,9 +0,0 @@
-class CreateLabels < ActiveRecord::Migration[5.2]
-  def change
-    create_table :labels do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190218025906_add_columns_to_labels.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToLabels < ActiveRecord::Migration[5.2]
-  def change
-    add_column :labels, :description, :string
-    add_column :labels, :color, :string
-  end
-end
