4e4772788b985b59e59a0248d7eb369d80508afa
tsubasa1122 <nakanot@open8.com>
2019-02-20 20:44:28 +0900

    CRUD処理の実装

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,52 @@
+class NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+    render json: @notes
+  end
+
+  def show
+    @note = Note.find(params[:id])
+    render json: @note
+  end
+
+  def new
+    @note = Note.new
+    # @labels = Label.all　ラベル追加時の為に残しておく
+    # render json: @labels
+  end
+
+  def create
+    note = Note.new(note_params)
+    if note.save
+      head :ok
+    else
+      head :bad_request
+    end
+  end
+
+  def update
+    note = Note.find(params[:id])
+    if note.updata(note_params)
+      head :ok
+    else
+      head :bad_request
+    end
+  end
+
+  def destroy
+    note = Note.find(params[:id])
+    if note.destroy
+      head :ok
+    else
+      head :bad_request
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+
+end
