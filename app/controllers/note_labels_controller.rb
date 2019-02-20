0d47d186a62e67fef7fd8420fbd62bf571b518ac
Hajime Todo <samsepylot@gmail.com>
2019-02-20 21:24:29 +0900

    Noteに対するLabelの追加と削除

---
+++ app/controllers/note_labels_controller.rb
@@ -0,0 +1,19 @@
+class NoteLabelsController < ApplicationController
+  def create
+    note.labels << label
+  end
+  
+  def destroy
+    note.labels.destroy(label)
+  end
+
+  private
+
+  def label
+    Label.find(params[:id])
+  end
+
+  def note
+    Note.find(params[:note_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get    'labels'      => 'labels#index'
-  get    'labels/:id'  => 'labels#show'
-  post   'labels'      => 'labels#create'
-  put    'labels/:id'  => 'labels#update'
-  delete 'labels/:id'  => 'labels#destroy'
+  get    'labels'               => 'labels#index'
+  get    'labels/:id'           => 'labels#show'
+  post   'labels'               => 'labels#create'
+  put    'labels/:id'           => 'labels#update'
+  delete 'labels/:id'           => 'labels#destroy'
+  put    'notes/:id/labels/:id' => 'note_labels#create'
+  delete 'notes/:id/labels/:id' => 'note_labels#destroy'
 end
