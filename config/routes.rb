bd98a1cbd1b2f55d89aaf66a348ff96f411e369a
Hajime Todo <samsepylot@gmail.com>
2019-02-22 11:52:13 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :notes, only: %i(index show create update destory)
+  resources :notes, only: %i(index show create update destory) do
+    resources :labels, only: %i(update destroy), controller: :note_labels
+  end
   resources :labels, only: %i(index show create update destory)
 end
