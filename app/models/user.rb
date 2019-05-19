c08b8849d74a432b5e7f6c17e41f797707483e35
tsubasa1122 <nakanot@open8.com>
2019-05-19 20:42:34 +0900

    save時に画像を生成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_one_attached :icon
+
+  after_update :process_image_convert
+
+  def process_image_convert
+    icon.variant(resize: '100x100').processed
+  end
 end
